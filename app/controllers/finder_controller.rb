27a29adeb7568f2b3eac7247e0da5daca47ea547
Marko Manojlovic <marko@thecookie.org>
2015-03-24 20:26:37 -0700

    Finished the missing email and alphabetized actions. They now use the index view.

--- app/controllers/finder_controller.rb
+++ app/controllers/finder_controller.rb
@@ -1,7 +1,19 @@
 class FinderController < ApplicationController
 	def index
+		@page_title = 'All Customers'
 		@customers = Customer.all
 	end
-	
-	
+
+	def alphabetized
+		@page_title = 'Alphabetized Customers'
+		@customers = Customer.order(:full_name)
+		render :index
+	end
+
+	def missing_email
+		@page_title = 'Customers Missing Email Addresses'
+		@customers = Customer.where(email_address: '')
+		render :index
+	end
+
 end
